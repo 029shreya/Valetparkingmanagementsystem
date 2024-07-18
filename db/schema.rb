ca49de86ce36333f4d7e4f45f884218937ba4c12
Shreya <shirkes008@gmail.com>
2024-07-18 15:44:37 +0530

    Create data of car and parking spot

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_18_062406) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_072328) do
+  create_table "cars", force: :cascade do |t|
+    t.string "license_plat_number"
+    t.string "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "parking_spots", force: :cascade do |t|
+    t.string "size"
+    t.string "status", default: "available"
+    t.integer "car_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
